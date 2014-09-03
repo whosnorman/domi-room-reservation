141f7c946c16af1783d2bd097072cd0fad791940
matt@domiventures.co <matt@domiventures.co>
2014-09-03 13:40:17 -0400

    get json

--- index.js
+++ index.js
@@ -23,9 +23,25 @@ app.post('/room', function(req, res) {
     console.log(body);
     console.log("ROOM POST\n");
     res.send(body);
+
+    schedule(body);
 });
 
 var port = Number(process.env.PORT || 5000);
 server.listen(port, function() {
    console.log("Listening on " + port);
 });
+
+
+function schedule(info){
+	var email = info.info;
+	var company = info.company;
+	var room = info.room;
+	var date = info.date;
+	var start = info.start;
+	var end = info.end;
+
+	console.log(email);
+	console.log(company);
+	console.log(room);
+};
