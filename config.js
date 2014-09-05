52f4eb47c7c3b27ac0a444de3b22636a68e8f920
matt@domiventures.co <matt@domiventures.co>
2014-09-04 20:55:13 -0400

    auth doesnt work

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	consumer_key : '129929270786-v8e3h1rkota9bskfk0a3e4gidobc2pn7.apps.googleusercontent.com',
+	consumer_secret : 'notasecret'
+}
--- index.js
+++ index.js
@@ -7,9 +7,13 @@ var logfmt = require("logfmt");
 var bodyParser = require('body-parser');
 
 var config = require('./config');
-var gcal = require('../GoogleCalendar');
+var gcal = require('google-calendar');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oatuh').OAuth2Strategy;
 
+var calID = 'domiventures.co_e1eknta8nrohjg1lhrqmntrla4@group.calendar.google.com';
 
+app.use(passpot.initialize());
 app.use(logfmt.requestLogger());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -21,19 +25,52 @@ app.get('/', function(req, res) {
 	res.sendfile(__dirname + '/public/index.html');
 }); */
 
+passport.use(new GoogleStrategy({
+	clientID: config.consumer_key,
+	clientSecret: config.consumer_secret,
+	callbackURL: "http://localhost:5000/auth/callback",
+	scope: ['openid', 'email', 'https://googleapis.com/auth/calendar']
+  },
+  function(accessToken, refreshToken, profile, done) {
+  	profile.accessToken = accessToken;
+  	return done(null, profile);
+  }
+));
+
+app.get('/auth', 
+	passport.authenticate('google', {session: false}));
+
+app.get('/auth/callback', 
+  passport.authenticate('google', { session: false, failureRedirect: '/login' }),
+  function(req, res) { 
+    req.session.access_token = req.user.accessToken;
+    res.redirect('/');
+  });
+
 app.post('/room', function(req, res) {
 	var body = req.body;
-    console.log(req);
+
     console.log(body);
     console.log("ROOM POST\n");
-    res.send(body);
 
-    schedule(body);
+    //schedule(body);
+
+    if(!req.session.access_token) return res.redirect('/auth');
+  
+	var accessToken     = req.session.access_token;
+	var text            = body.room + ' ' + body.company;
+
+	gcal(accessToken).events.quickAdd(calID, text, function(err, data) {
+		if(err) return res.send(500,err);
+		return res.redirect('/');
+	});
+
+	res.send(body);
 });
 
 var port = Number(process.env.PORT || 5000);
 server.listen(port, function() {
-   console.log("Listening on " + port);
+   console.log("Listenin\' on " + port);
 });
 
 
@@ -49,3 +86,19 @@ function schedule(info){
 	console.log(company);
 	console.log(room);
 };
+
+/*
+app.all('/add', function(req, res){
+  
+  if(!req.session.access_token) return res.redirect('/auth');
+  
+  var accessToken     = req.session.access_token;
+  var calendarId      = calID;
+  var text            = req.query.text || 'Hello World';
+  
+  gcal(accessToken).events.quickAdd(calendarId, text, function(err, data) {
+    if(err) return res.send(500,err);
+    return res.redirect('/');
+  });
+});
+*/
