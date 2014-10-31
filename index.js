d5a6e9bd3bdb4560bcbc82b9a94763b4cc9a87b4
matt@domiventures.co <matt@domiventures.co>
2014-10-31 13:04:18 -0400

    working on error email

--- index.js
+++ index.js
@@ -273,7 +273,7 @@ function sendEmail(user, ev){
   var send_at = null;               // have to pay to get this feature
 
   mandrillClient.messages.send({"message": message, "async": async, "ip_pool": ip_pool, "send_at": send_at}, function(result) {
-      console.log('EMAIL RESULT:');
+      console.log('--EMAIL RESULT--:');
       console.log(result);
       /*      example log
       [{
@@ -286,6 +286,113 @@ function sendEmail(user, ev){
   }, function(e) {
       // Mandrill returns the error as an object with name and message keys
       console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+      sendErrMail(var err = {name: e.name, message: e.message});
+      // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+  });
+
+};
+
+
+
+
+function sendErrMail(err){
+  var myEamil = "matt@domiventures.co";
+  console.log('--ERROR BELOW--');
+  console.log(err);
+
+  var now = moment().format();
+
+  var msg = "<body style='color: #303030 !important;'><h1>There was an error!</h1><br /><p>" + err + "</p><br /><p>" + now + "</p></body>";
+
+  var message = {
+      "html": msg,
+      "text": null,
+      "subject": "Snag A Room",
+      "from_email": myEmail,
+      "from_name": "Domi Station",
+      "to": [{
+              "email": myEmail,
+              "name": 'Server Errors',
+              "type": "to"
+          }],
+      "headers": {
+          "Reply-To": myEamil
+      },
+      "important": false,
+      "track_opens": true,
+      "track_clicks": null,
+      "auto_text": null,
+      "auto_html": null,
+      "inline_css": null,
+      "url_strip_qs": null,
+      "preserve_recipients": null,
+      "view_content_link": null,
+      "bcc_address": null,            // possibly add one?
+      "tracking_domain": null,
+      "signing_domain": null,
+      "return_path_domain": null,
+      "merge": false
+      /*
+      "global_merge_vars": [{
+              "name": "merge1",
+              "content": "merge1 content"
+          }],
+      "merge_vars": [{
+              "rcpt": "recipient.email@example.com",
+              "vars": [{
+                      "name": "merge2",
+                      "content": "merge2 content"
+                  }]
+          }],
+      "tags": [
+          "password-resets"
+      ],
+      "subaccount": "customer-123",
+      "google_analytics_domains": [
+          "example.com"
+      ],
+      "google_analytics_campaign": "message.from_email@example.com",
+      "metadata": {
+          "website": "www.example.com"
+      },
+      "recipient_metadata": [{
+              "rcpt": "recipient.email@example.com",
+              "values": {
+                  "user_id": 123456
+              }
+          }],
+      "attachments": [{
+              "type": "text/plain",
+              "name": "myfile.txt",
+              "content": "ZXhhbXBsZSBmaWxl"
+          }],
+      "images": [{
+              "type": "image/png",
+              "name": "IMAGECID",
+              "content": "ZXhhbXBsZSBmaWxl"
+          }]
+          */
+  };
+
+  var async = false;
+  var ip_pool = "Main Pool";        // resets to default ip pool
+  var send_at = null;               // have to pay to get this feature
+
+  mandrillClient.messages.send({"message": message, "async": async, "ip_pool": ip_pool, "send_at": send_at}, function(result) {
+      console.log('--ERR EMAIL RESULT--:');
+      console.log(result);
+      /*      example log
+      [{
+              "email": "recipient.email@example.com",
+              "status": "sent",
+              "reject_reason": "hard-bounce",
+              "_id": "abc123abc123abc123abc123abc123"
+          }]
+      */
+  }, function(e) {
+      // Mandrill returns the error as an object with name and message keys
+      console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+      sendErrMail(var err = {name: e.name, message: e.message});
       // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
   });
 
