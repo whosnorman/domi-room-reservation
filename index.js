4b53c43d01873fdd2a92e71a0decfe3e9fc604f7
matt@domiventures.co <matt@domiventures.co>
2014-11-19 21:29:23 -0500

    added loading bar

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
