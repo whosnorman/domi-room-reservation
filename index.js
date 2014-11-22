f62b0160f9163d89cd7f00fdfb99d9c60edc4a29
matt@domiventures.co <matt@domiventures.co>
2014-11-21 21:10:03 -0500

    added back /public to app use

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
 app.get('/robots.txt', function(req, res) {
   res.type('text/plain')
   res.send("User-agent: *\nDisallow: /");
