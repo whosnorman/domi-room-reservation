6b38968eebfabb31d0a0bb3d51012b5cd78411f4
Matt O'Hagan <mattohagan@Matts-MacBook-Pro.local>
2015-11-02 15:15:29 -0500

    redo origin header log

--- boot/config.js
+++ boot/config.js
@@ -23,9 +23,8 @@ module.exports = function(app){
 	// set headers
 	var allowCrossDomain = function(req, res, next) {
 		console.log('--- HEADER ORIGIN ---');
-		console.log(req.get('host'));
-		console.log(req.header);
-		console.log(req.header.referers);
+		console.log(req.get('origin'));
+		console.log(req.headers);
 	    res.header('Access-Control-Allow-Origin', '*');
 	    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
 	    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
